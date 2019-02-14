6fd0be62cbb284227b6f4e3e3869d03269d7be6d
Amado <amadogr50@gmail.com>
2019-02-14 17:00:12 -0600

    MainActivity renamed for AddStudentActivity.

--- app/src/main/java/com/marito/sesion5/AddStudentActivity.java
+++ app/src/main/java/com/marito/sesion5/AddStudentActivity.java
@@ -40,7 +40,7 @@ public class AddStudentActivity extends AppCompatActivity {
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_add_new_student);
     
     name = findViewById(R.id.name);
     phone = findViewById(R.id.phone);
